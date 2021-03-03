60761fc842b6298000df17b436150d03bcefc2b4
FrancoisBotha <francois@ims.com.na>
2021-03-03 12:51:31 +1100

    Add users endpoint

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const cool = require('cool-ascii-faces');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -8,5 +7,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
+  .get('/users', (req, res) => res.send('testing'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
